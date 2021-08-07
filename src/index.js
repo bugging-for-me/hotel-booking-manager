8b630c29deb5fedce5eb0648fd80cb32fbc3ad33
nguyenhuubinh2001 <nguyenbinh11b7@gmail.com>
2021-08-07 23:28:33 +0700

    nguyenbinh

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Header from './components/user/Header';
 ReactDOM.render(
   <React.StrictMode>
-    <Header />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
