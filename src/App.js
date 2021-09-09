c37a61ad29a592971fa00bbaad6d7ddcf59a59f1
sangbx <sangbx@d2t.vn>
2021-09-09 15:41:44 +0700

    sangbx add component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import Header from './components/user/header/Header';
+import MenuBar from './components/user/header/MenuBar';
 import logo from './logo.svg';
 import './App.css';
 import Slider from './components/user/Slider';
+import Footer from './components/user/footer/Footer';
+import Main from './components/user/main/Main';
+import Objectives from './components/user/objectives/Objectives';
+import Poster from './components/user/poster/Poster'
 function App() {
   return (
-    <Slider />
+    <div >
+      <Header />
+      <MenuBar />
+      <Footer/>
+    </div>
   );
 }
 
